4c884e31a56c72ca2ca1bb6264e6c6c2b6d6da20
Skycloudd <51929172+Skycloudd@users.noreply.github.com>
2020-07-07 17:27:38 +0200

    fixed trig commands

--- cogs/math.py
+++ cogs/math.py
@@ -12,15 +12,45 @@ class Math(commands.Cog):
 		try:
 			x = float(x)
 		except ValueError:
-			return ctx.send(f'{x} is not a number')
+			return await ctx.send(f'{x} is not a number')
 
 		if rad_deg == 'rad':
-			return ctx.send(f'sin({x}) = {math.sin(x)}')
+			return await ctx.send(f'sin({x}) = {math.sin(x)} in radians')
 		elif rad_deg == 'deg':
-			return ctx.send(f'sin({x}) = {math.degrees(math.sin(x))}')
+			return await ctx.send(f'sin({x}) = {math.degrees(math.sin(x))} in degrees')
 
 		elif rad_deg != 'rad' and rad_deg != 'deg':
-			return ctx.send('second argument must be either \'rad\' or \'deg\'')
+			return await ctx.send('second argument must be either \'rad\' or \'deg\'')
+
+	@commands.command(description='Calculates the cosine of an angle in radians or degrees')
+	async def cos(self, ctx, x, rad_deg):
+		try:
+			x = float(x)
+		except ValueError:
+			return await ctx.send(f'{x} is not a number')
+
+		if rad_deg == 'rad':
+			return await ctx.send(f'cos({x}) = {math.cos(x)} in radians')
+		elif rad_deg == 'deg':
+			return await ctx.send(f'cos({x}) = {math.degrees(math.cos(x))} in degrees')
+
+		elif rad_deg != 'rad' and rad_deg != 'deg':
+			return await ctx.send('second argument must be either \'rad\' or \'deg\'')
+
+	@commands.command(description='Calculates the tangent of an angle in radians or degrees')
+	async def tan(self, ctx, x, rad_deg):
+		try:
+			x = float(x)
+		except ValueError:
+			return await ctx.send(f'{x} is not a number')
+
+		if rad_deg == 'rad':
+			return await ctx.send(f'tan({x}) = {math.tan(x)} in radians')
+		elif rad_deg == 'deg':
+			return await ctx.send(f'tan({x}) = {math.degrees(math.tan(x))} in degrees')
+
+		elif rad_deg != 'rad' and rad_deg != 'deg':
+			return await ctx.send('second argument must be either \'rad\' or \'deg\'')
 
 
 def setup(bot):
