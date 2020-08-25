a66427a65302b41ba69058de7fb9a4603da8393f
Skycloudd <51929172+Skycloudd@users.noreply.github.com>
2020-08-25 21:54:52 +0200

    updated prefixes

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ start = time.time()
 def get_prefix(bot, message):
 	"""A callable Prefix for our bot. This could be edited to allow per server prefixes."""
 
-	prefixes = ['msd ', 'msdbot ', '?', '!', '@', ';', '.']
+	prefixes = ['msd ', 'max ', '?', '!', '@', ';', '.']
 
 	# Check to see if we are outside of a guild. e.g DM's etc.
 	# if not message.guild:
