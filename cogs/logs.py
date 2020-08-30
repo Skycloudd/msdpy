c93bddd32a9986bf70c9c07bd299e254236778b9
Skycloudd <51929172+Skycloudd@users.noreply.github.com>
2020-08-30 18:17:09 +0200

    fixed logs

--- cogs/logs.py
+++ cogs/logs.py
@@ -12,25 +12,15 @@ class Logs(commands.Cog):
 
 		channel = self.bot.get_channel(int(self.bot.config[str(msg.guild.id)]["log_channel"]))
 
-		self.bot.messageBlacklist.remove(message.id)
-			return
-		if message.guild.id != 574267523869179904:
-			return 
-		if message.author.color.value == 0:
-			color = 16777210
-		else:
-			color = message.author.color
-		channel = self.bot.get_channel(int(self.bot.config[str(message.guild.id)]["logs_channel"]))
 		embed = discord.Embed(
 			title='Deleted Message',
-			color=color,
-			timestamp=message.created_at
+			color=16777210,
+			timestamp=msg.created_at
 		)
-		embed.add_field(
-			name='User', value=message.author.mention, inline=True)
-		embed.add_field(
-			name='Channel', value=message.channel.mention, inline=True)
-		embed.add_field(name='Message', value=message.content, inline=False)
+		embed.add_field(name='User', value=msg.author.mention, inline=True)
+		embed.add_field(name='Channel', value=msg.channel.mention, inline=True)
+		embed.add_field(name='Message', value=msg.content, inline=False)
+
 		await channel.send(embed=embed)
 
 	@commands.Cog.listener()
@@ -38,24 +28,20 @@ class Logs(commands.Cog):
 		if before.content == after.content:
 			return
 
-		if after.guild.id != 574267523869179904:
-			return 
-		channel = self.bot.get_channel(int(self.bot.config[str(before.guild.id)]["logs_channel"]))
-		if before.author.color.value == 0:
-			color = 16777210
-		else:
-			color = before.author.color
+		channel = self.bot.get_channel(int(self.bot.config[str(after.guild.id)]["log_channel"]))
+
 		embed = discord.Embed(
 			title='Edited Message',
-			color=color,
+			color=16777210,
 			timestamp=after.edited_at
 		)
-		embed.add_field(
-			name='User', value=before.author.mention, inline=True)
-		embed.add_field(
-			name='Channel', value=before.channel.mention, inline=True)
-		embed.add_field(name='Original Message',
-						value=before.content, inline=False)
+		embed.add_field(name='User', value=before.author.mention, inline=True)
+		embed.add_field(name='Channel', value=before.channel.mention, inline=True)
+		embed.add_field(name='Original Message', value=before.content, inline=False)
 		embed.add_field(name='New Message', value=after.content, inline=False)
+
 		await channel.send(embed=embed)
+
+
 def setup(bot):
+	bot.add_cog(Logs(bot))
