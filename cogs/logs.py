773a58be3c57827cebbf01052b06f32b51da8fe9
Spnwztkn <68794897+Spnwztkn@users.noreply.github.com>
2020-08-29 20:58:41 -0400

    hi
    skye asked for me to do so i did

--- cogs/logs.py
+++ cogs/logs.py
@@ -12,38 +12,50 @@ class Logs(commands.Cog):
 
 		channel = self.bot.get_channel(int(self.bot.config[str(msg.guild.id)]["log_channel"]))
 
+		self.bot.messageBlacklist.remove(message.id)
+			return
+		if message.guild.id != 574267523869179904:
+			return 
+		if message.author.color.value == 0:
+			color = 16777210
+		else:
+			color = message.author.color
+		channel = self.bot.get_channel(int(self.bot.config[str(message.guild.id)]["logs_channel"]))
 		embed = discord.Embed(
 			title='Deleted Message',
-			color=16777210,
-			timestamp=msg.created_at
+			color=color,
+			timestamp=message.created_at
 		)
-		embed.add_field(name='User', value=msg.author.mention, inline=True)
-		embed.add_field(name='Channel', value=msg.channel.mention, inline=True)
-		embed.add_field(name='Message', value=msg.content, inline=False)
-
+		embed.add_field(
+			name='User', value=message.author.mention, inline=True)
+		embed.add_field(
+			name='Channel', value=message.channel.mention, inline=True)
+		embed.add_field(name='Message', value=message.content, inline=False)
 		await channel.send(embed=embed)
 
 	@commands.Cog.listener()
 	async def on_message_edit(self, before, after):
+		if before.content == after.content:
+			return
 
-		channel = self.bot.get_channel(int(self.bot.config[str(after.guild.id)]["log_channel"]))
-
-		try:
-			embed = discord.Embed(
-				title='Edited Message',
-				color=16777210,
-				timestamp=after.edited_at
-			)
-			embed.add_field(name='User', value=before.author.mention, inline=True)
-			embed.add_field(name='Channel', value=before.channel.mention, inline=True)
-			embed.add_field(name='Original Message', value=before.content, inline=False)
-			embed.add_field(name='New Message', value=after.content, inline=False)
-
-			await channel.send(embed=embed)
-
-		except TypeError:
-			print("TypeError in logging")
-
-
+		if after.guild.id != 574267523869179904:
+			return 
+		channel = self.bot.get_channel(int(self.bot.config[str(before.guild.id)]["logs_channel"]))
+		if before.author.color.value == 0:
+			color = 16777210
+		else:
+			color = before.author.color
+		embed = discord.Embed(
+			title='Edited Message',
+			color=color,
+			timestamp=after.edited_at
+		)
+		embed.add_field(
+			name='User', value=before.author.mention, inline=True)
+		embed.add_field(
+			name='Channel', value=before.channel.mention, inline=True)
+		embed.add_field(name='Original Message',
+						value=before.content, inline=False)
+		embed.add_field(name='New Message', value=after.content, inline=False)
+		await channel.send(embed=embed)
 def setup(bot):
-	bot.add_cog(Logs(bot))
