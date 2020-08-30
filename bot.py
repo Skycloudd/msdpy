9a620d98fca41f55caac199217696d695c037acf
Skycloudd <51929172+Skycloudd@users.noreply.github.com>
2020-08-30 02:29:00 +0200

    removed dotenv, also going to sleep now

--- bot.py
+++ bot.py
@@ -7,8 +7,6 @@ import aiohttp
 import discord
 from discord.ext import commands
 
-from dotenv import load_dotenv
-
 extensions = [
 	"cogs.player",
 	"cogs.general",
@@ -18,9 +16,6 @@ extensions = [
 	"cogs.math",
 	"cogs.eval"
 ]
-
-load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
 
 start = time.time()
 
