5887e65c17449e202aa15ebf4572504fe8dbb3a7
Skycloudd <51929172+Skycloudd@users.noreply.github.com>
2020-09-06 13:33:39 +0200

    set logging level to debug

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def setup_logging():
 	DATE_FORMAT = '%d/%m/%Y (%H:%M:%S)'
 
 	logger = logging.getLogger('discord')
-	logger.setLevel(logging.ERROR)
+	logger.setLevel(logging.DEBUG)
 
 	file_handler = logging.FileHandler(filename='discord.log', mode='a', encoding='utf-8')
 	file_handler.setFormatter(logging.Formatter(fmt=FORMAT, datefmt=DATE_FORMAT))
